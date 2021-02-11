8e0e076624d25c99a3ea91fedeb0cc05f7a32654
Andrew Lisowski <lisowski54@gmail.com>
2021-02-11 14:38:15 -0800

    fix extra space in sparse RepoDescription

--- src/components/RepoDescription.tsx
+++ src/components/RepoDescription.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import { Flex, Text, AvatarStack } from "@primer/components";
-import { StarIcon } from "@primer/styled-octicons";
 import { Actor, ExtendedRepo } from "../utils/types";
 import { RepoLink } from "./HomePageLink";
 import { renderEmoji } from "../utils/renderEmoji";
@@ -17,33 +16,42 @@ export const RepoDescription = ({
   repo: ExtendedRepo;
   users?: Actor[];
 } & React.ComponentProps<typeof Flex>) => {
+  const hasRepoInfo =
+    repo.languages?.edges[0] || repo.stargazers || users.length > 0;
+
   return (
     <Flex flexDirection="column" {...props}>
       <RepoLink repo={repo} mb={1} />
-      <Text mb={2} color="gray.7">
-        {renderEmoji(repo.description)}
-      </Text>
-      <Flex alignItems="center" justifyContent="space-between">
-        <Flex>
-          {repo.languages?.edges[0] && (
-            <Language language={repo.languages.edges[0]} mr={4} />
-          )}
-          {repo.stargazers && (
-            <StarCount
-              stargazers={repo.stargazers.totalCount}
-              repo={repo.url}
-            />
+
+      {repo.description && (
+        <Text mb={2} color="gray.7">
+          {renderEmoji(repo.description)}
+        </Text>
+      )}
+
+      {hasRepoInfo && (
+        <Flex alignItems="center" justifyContent="space-between">
+          <Flex>
+            {repo.languages?.edges[0] && (
+              <Language language={repo.languages.edges[0]} mr={4} />
+            )}
+            {repo.stargazers && (
+              <StarCount
+                stargazers={repo.stargazers.totalCount}
+                repo={repo.url}
+              />
+            )}
+          </Flex>
+
+          {users.length > 0 && (
+            <AvatarStack alignRight>
+              {users.map((user) => (
+                <ActorAvatar key={user.id} showTooltip actor={user} size={20} />
+              ))}
+            </AvatarStack>
           )}
         </Flex>
-
-        {users.length > 0 && (
-          <AvatarStack alignRight>
-            {users.map((user) => (
-              <ActorAvatar key={user.id} showTooltip actor={user} size={20} />
-            ))}
-          </AvatarStack>
-        )}
-      </Flex>
+      )}
     </Flex>
   );
 };
